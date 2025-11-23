84c3c38cfb87cb651213021fdd825345d85af307
Cunningham Li <2231358@champlaincollege.qc.ca>
2025-11-22 21:41:32 -0500

    Update adafruit_client.py

--- flask_dashboard/adafruit_client.py
+++ flask_dashboard/adafruit_client.py
@@ -1,1 +1,24 @@
+import os
+import requests
+
+AIO_USER = os.getenv("AIO_USERNAME")
+AIO_KEY = os.getenv("AIO_KEY")
+
+HEADERS = {"X-AIO-Key": AIO_KEY}
+
+def get_latest(feed_key: str):
+    url = f"https://io.adafruit.com/api/v2/{AIO_USER}/feeds/{feed_key}/data/last"
+    r = requests.get(url, headers=HEADERS, timeout=5)
+    r.raise_for_status()
+    return r.json()["value"]
+
+def publish_value(feed_key: str, value):
+    """
+    Push value to a feed.
+    """
+    url = f"https://io.adafruit.com/api/v2/{AIO_USER}/feeds/{feed_key}/data"
+    payload = {"value": value}
+    r = requests.post(url, json=payload, headers=HEADERS, timeout=5)
+    r.raise_for_status()
+    return r.json()
 
